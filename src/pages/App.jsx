4461d905daf9f219573c2149cd70cb5c06467e49
Bianca Viana <bianca.albuquerquev@hotmail.com>
2024-05-22 22:40:34 -0300

    corrigindo CRLF  warnings

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -3,7 +3,6 @@ import { GlobalStyle } from "./style";
 import { Component } from "./style";
 
 import { Header } from "../components/Header";
-import { Button } from "../components/Button";
 import { Footer } from "../components/Footer";
 
 export function App() {
